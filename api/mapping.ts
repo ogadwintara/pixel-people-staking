e01b5f4f453be77a63c39c4743d2278e3b283e23
triiq <87587418+triiq@users.noreply.github.com>
2022-05-30 21:31:55 -0700

    initial commit

---
+++ api/mapping.ts
@@ -0,0 +1,244 @@
+import { AirdropMetadata } from './../common/Airdrop'
+import { PublicKey } from '@solana/web3.js'
+import { ReceiptType } from '@cardinal/staking/dist/cjs/programs/stakePool'
+
+export type StakePoolMetadata = {
+  // Name of this stake pool used as an id. Should be in lower-case kebab-case since it is used in the URL as /{name}
+  // https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/Why-you-should-make-kebab-case-a-URL-naming-convention-best-practice
+  name: string
+  // Display name to be displayed in the header. Often the same as name but with capital letters and spaces
+  displayName: string
+  // Publickey for this stake pool
+  stakePoolAddress: PublicKey
+  // Default receipt type. Setting this will remove the option for the user to choose which receipt type to use
+  receiptType?: ReceiptType
+  // Optional config to hide this pool from the main page
+  hidden?: boolean
+  // Colors object to style the stake page
+  colors?: {
+    primary: string
+    secondary: string
+    accent?: string
+    fontColor?: string
+  }
+  // Image url to be used as the icon in the pool selector and the header
+  imageUrl?: string
+  // Website url if specified will be navigated to when the image in the header is clicked
+  websiteUrl?: string
+  // Max staked is used to compute percentage of total staked
+  maxStaked?: number
+  // Links to show at the top right of the page
+  links?: { text: string; value: string }[]
+  // On devnet when you click the airdrop button on this page it will clone NFTs with this metadata and airdrop to the user
+  airdrops?: AirdropMetadata[]
+}
+
+export const defaultSecondaryColor = 'rgba(29, 78, 216, 255)'
+
+export const stakePoolMetadatas: StakePoolMetadata[] = [
+  {
+    name: 'cardinal',
+    displayName: 'Cardinal',
+    stakePoolAddress: new PublicKey(
+      '2s3qXuGyMNedXS61Vi9XsRx7HuryyyZUYGyMtCrKUXva'
+    ),
+    imageUrl: '/logo-colored.png',
+    colors: {
+      primary: 'rgb(54,21,38,0.9)',
+      secondary: 'rgb(157,120,138, 0.6)',
+    },
+  },
+  {
+    name: 'blockasset',
+    displayName: 'Blockasset',
+    stakePoolAddress: new PublicKey(
+      '3BZCupFU6X3wYJwgTsKS2vTs4VeMrhSZgx4P2TfzExtP'
+    ),
+    websiteUrl: 'https://hub.blockasset.co/nft-staking-v2',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://blockasset.co/static/logo-e51ac9985ba7aef4ac8c1b1ae1c00511.png',
+    maxStaked: 11791,
+    colors: {
+      primary: '#000000',
+      secondary: '#4da1de',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+    airdrops: [
+      {
+        name: 'Blockasset Legends',
+        symbol: 'LEGENDS',
+        uri: 'https://arweave.net/Q5y8_OehSOYCkGiX-hV1H6qiczoDaVdk4Eyi4lhhdQE',
+      },
+    ],
+  },
+  {
+    name: 'blockasset-smesh-bros',
+    displayName: 'Blockasset Smesh Bros',
+    stakePoolAddress: new PublicKey(
+      'Bce4Aq4YheBo5hENeoMhjywdvMhEMc8sUh21S87Qv4q6'
+    ),
+    websiteUrl: 'https://hub.blockasset.co/smesh-staking',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://blockasset.co/static/logo-e51ac9985ba7aef4ac8c1b1ae1c00511.png',
+    maxStaked: 4000,
+    colors: {
+      primary: '#000000',
+      secondary: '#4da1de',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+    airdrops: [
+      {
+        name: 'Blockasset Legends',
+        symbol: 'LEGENDS',
+        uri: 'https://arweave.net/Q5y8_OehSOYCkGiX-hV1H6qiczoDaVdk4Eyi4lhhdQE',
+      },
+    ],
+  },
+  {
+    name: 'meta-ops',
+    displayName: 'MetaOps PFP Vault',
+    stakePoolAddress: new PublicKey(
+      'FP9BRAohGJDximSTa9HR3UNCd9KA5QUApyctMpASrsJp'
+    ),
+    websiteUrl: 'https://metaopsgaming.com/',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://www.metaopsvault.com/img/staking_hub_header_logo.0b7bc420.png',
+    maxStaked: 5555,
+    links: [
+      {
+        text: "Founder's Passes",
+        value: '/meta-ops-founders-vault',
+      },
+    ],
+    colors: {
+      primary: '#000000',
+      secondary: '#4da1de',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+  },
+  {
+    name: 'meta-ops-founders-vault',
+    displayName: 'MetaOps Founders Passes Vault',
+    stakePoolAddress: new PublicKey(
+      'BeunK2rKRNXbL6YsMkKDPD4f24Ms4dcj2JpsN6KCjBjY'
+    ),
+    websiteUrl: 'https://metaopsgaming.com/',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://www.metaopsvault.com/img/staking_hub_header_logo.0b7bc420.png',
+    maxStaked: 2000,
+    links: [
+      {
+        text: 'PFP Vault',
+        value: '/meta-ops',
+      },
+    ],
+    colors: {
+      primary: '#000000',
+      secondary: '#4da1de',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+  },
+  {
+    name: 'okaybulls',
+    displayName: 'Okaybulls',
+    stakePoolAddress: new PublicKey(
+      '34Mu6xQSWzJDwyXrQcbmuRA6JjJQEWwwzhFubmrGD2qx'
+    ),
+    websiteUrl: 'https://okaybulls.com/',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://rawcdn.githack.com/okaybulls/token/a3dc077179ac9e3f0aa1a784ef839af0f35e3f2e/bull.png',
+    maxStaked: 10000,
+    colors: {
+      primary: '#1C1C1C',
+      secondary: '#F4431C',
+      accent: '#434343',
+      fontColor: '#FFFFFF',
+    },
+  },
+  {
+    name: 'friendly-frogs',
+    displayName: 'Friendly Frogs',
+    stakePoolAddress: new PublicKey(
+      'AHighyKxRsD6oo6SebbW6nQfuJ1GQBSmU2BnVFbtcFmz'
+    ),
+    websiteUrl: 'https://ffsc.io/',
+    receiptType: ReceiptType.Original,
+    maxStaked: 2121,
+    imageUrl: 'https://arweave.net/MYKL6LSm8JFdqqfARCDqpmFBUlfkRplneVgIkBqyZE4',
+    colors: {
+      primary: '#698a5e',
+      secondary: '#74c48e',
+      fontColor: '#f2edd4',
+    },
+  },
+  {
+    name: 'gemmy',
+    displayName: 'Gemmy',
+    stakePoolAddress: new PublicKey(
+      'GFT4PQfgB1ySCr826GhdstzTMndwvoqkBJuZsG7Uxrw1'
+    ),
+    websiteUrl: 'https://gemmy.club/',
+    receiptType: ReceiptType.Original,
+    maxStaked: 5000,
+    imageUrl: 'https://arweave.net/sjCF_O89hlwQkMts0XHxoLSXGqVdZfy7PlymRvh_FgY',
+    colors: {
+      primary: '#7D89D8',
+      secondary: '##131418',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+  },
+  {
+    name: 'seanies',
+    displayName: 'Seanies',
+    stakePoolAddress: new PublicKey(
+      'EWfauqUC6PQTzXGR4h4LT1KMqd28W8Vjk5XLPfkLFeSA'
+    ),
+    websiteUrl: 'https://seanies.art/',
+    receiptType: ReceiptType.Original,
+    imageUrl:
+      'https://raw.githubusercontent.com/dickmasterson/seanies/master/sean-logo.png',
+  },
+  {
+    name: 'whales',
+    displayName: 'Catalina Whales',
+    stakePoolAddress: new PublicKey(
+      'BrR1W8bNBfJZGqzsSvMQ8tUJL9tm963E6qR7R99YReiD'
+    ),
+    hidden: true,
+    receiptType: ReceiptType.Original,
+    imageUrl: '/logos/whales.jpg',
+    colors: {
+      primary: '#2472d1',
+      secondary: '#eee',
+      accent: '#999',
+      fontColor: '#000',
+    },
+  },
+  {
+    name: 'stoned-farms',
+    displayName: 'Stoned Farms',
+    stakePoolAddress: new PublicKey(
+      'BdqcbcwaX5YpQPDLh9m9u89QH46WXcnQB5r7vK3h54U4'
+    ),
+    websiteUrl: 'https://stonedfarms.io/',
+    receiptType: ReceiptType.Original,
+    maxStaked: 2500,
+    imageUrl: 'https://smvkptoniao6opm5dr3gwgm45tyk3hk5dhz4vrsjqyviqqlkwu.arweave.net/k_yqnzc1AHec9nRx2axmc7PCtnV0Z88rGSYYqiEFqtU',
+    colors: {
+      primary: '#344E41',
+      secondary: '#131418',
+      accent: '#1fcfb11c',
+      fontColor: '#FFFFFF',
+    },
+  },
+]

90ccb218730d50dd440b2ecd0ea6d2b61cbbf4b4
ogadwintara <dwintaraoga@gmail.com>
2022-09-06 14:41:39 +0700

    change rpc

--- api/constants.tsx
+++ api/constants.tsx
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const STAKE_POOL_ID = new PublicKey("DNHGMhkRiFZsWXGVKHbQVctvDtK7MEvGU9u4vMQ2Qmsh")
+export const STAKE_POOL_ID = new PublicKey("8P8GfwZ2SbqxP8KBNo5SvGajFNKEUf4yWNpaKbpfPWkv")
